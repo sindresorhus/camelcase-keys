8a6c617b1ccd2a9bd934540e98929b0561d1b79c
Sindre Sorhus <sindresorhus@gmail.com>
2020-03-04 00:47:05 +0800

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,22 +1,22 @@
 import {expectType} from 'tsd';
 import camelcaseKeys = require('.');
 
-expectType<Array<{ [key in 'foo-bar']: true }>>(camelcaseKeys([{'foo-bar': true}]));
+expectType<Array<{[key in 'foo-bar']: true}>>(camelcaseKeys([{'foo-bar': true}]));
 
-expectType<{ [key in 'foo-bar']: true }>(camelcaseKeys({'foo-bar': true}));
+expectType<{[key in 'foo-bar']: true}>(camelcaseKeys({'foo-bar': true}));
 
-expectType<{ [key in 'foo-bar']: true }>(
+expectType<{[key in 'foo-bar']: true}>(
 	camelcaseKeys({'foo-bar': true}, {deep: true}),
 );
 
-expectType<{ [key in 'foo-bar']: true }>(
+expectType<{[key in 'foo-bar']: true}>(
 	camelcaseKeys({'foo-bar': true}, {deep: true, pascalCase: true}),
 );
 
-expectType<{ [key in 'foo-bar']: true }>(
+expectType<{[key in 'foo-bar']: true}>(
 	camelcaseKeys({'foo-bar': true}, {exclude: ['foo', /bar/]}),
 );
 
-expectType<{ [key in 'foo-bar']: true }>(
+expectType<{[key in 'foo-bar']: true}>(
 	camelcaseKeys({'foo-bar': true}, {stopPaths: ['foo']}),
 );
