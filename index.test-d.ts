1fa6ce23100fb0f98f13f129532cbae9fc62429d
Justin Abene <jabene@gmail.com>
2020-03-04 08:08:57 -0500

    Add var assignment tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,6 +1,14 @@
 import {expectType} from 'tsd';
 import camelcaseKeys = require('.');
 
+const fooBarObj = {'foo-bar': true}
+const camelFooBarObj = camelcaseKeys(fooBarObj)
+expectType<typeof fooBarObj>(camelFooBarObj)
+
+const fooBarArr = [{'foo-bar': true}]
+const camelFooBarArr = camelcaseKeys(fooBarArr)
+expectType<typeof fooBarArr>(camelFooBarArr)
+
 expectType<Array<{[key in 'foo-bar']: true}>>(camelcaseKeys([{'foo-bar': true}]));
 
 expectType<{[key in 'foo-bar']: true}>(camelcaseKeys({'foo-bar': true}));
