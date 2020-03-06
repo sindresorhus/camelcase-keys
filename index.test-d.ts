0d973543ba33bb87127af0bab00c9142900ecc4d
Håkon Drolsum Røkenes <hakon.rokenes@niva.no>
2020-03-06 12:49:59 +0100

    Changed returned T extends {[key: string]: any} (instead of unknown) #55
    motivation: this way the library is less restrictive and should be
    easier to adopt to existing codebases.
    
    We only really care about the keys in our objects being strings (as they
    should be).

--- index.test-d.ts
+++ index.test-d.ts
@@ -28,3 +28,25 @@ expectType<{[key in 'foo-bar']: true}>(
 expectType<{[key in 'foo-bar']: true}>(
 	camelcaseKeys({'foo-bar': true}, {stopPaths: ['foo']}),
 );
+
+interface SomeObject {
+	someProperty: string;
+}
+
+const someObj: SomeObject = {
+	someProperty: "hello",
+};
+
+expectType<SomeObject>(camelcaseKeys(someObj));
+expectType<SomeObject[]>(camelcaseKeys([someObj]));
+
+type SomeTypeAlias = {
+	someProperty: string;
+}
+
+const objectWithTypeAlias = {
+	someProperty: "this should also work"
+};
+
+expectType<SomeTypeAlias>(camelcaseKeys(objectWithTypeAlias));
+expectType<SomeTypeAlias[]>(camelcaseKeys([objectWithTypeAlias]));
